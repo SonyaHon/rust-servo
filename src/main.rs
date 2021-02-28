b5ec41507d24e97de9b0732d98d0c9d8693f37a5
sonyahon <volkov030@gmail.com>
2021-02-28 21:17:51 +0300

    swap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use rust_gpiozero::Servo;
 
 fn main() {
-    let mut leg_servo_bottom = Servo::new(8);
-    let mut leg_servo_top = Servo::new(10);
+    let mut leg_servo_bottom = Servo::new(10);
+    let mut leg_servo_top = Servo::new(8);
 
     leg_servo_bottom.mid();
 }
