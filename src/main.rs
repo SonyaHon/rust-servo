1b67d8c1c6ff8b15b225acb6ee22c65fb4e6af21
sonyahon <volkov030@gmail.com>
2021-02-28 21:22:05 +0300

    try 14

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rust_gpiozero::Servo;
 
 fn main() {
-    let mut leg_servo_bottom = Servo::new(10);
+    let mut leg_servo_bottom = Servo::new(14);
     let mut leg_servo_top = Servo::new(8);
 
     leg_servo_bottom.mid();
