1f4c43123e564901e32d56f650f0313e75cd927f
sonyahon <volkov030@gmail.com>
2021-02-28 21:16:43 +0300

    fix borrow issue

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use rust_gpiozero::Servo;
 
 fn main() {
-    let leg_servo_bottom = Servo::new(8);
-    let leg_servo_top = Servo::new(10);
+    let mut leg_servo_bottom = Servo::new(8);
+    let mut leg_servo_top = Servo::new(10);
 
     leg_servo_bottom.mid();
 }
